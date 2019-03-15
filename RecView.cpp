4d6d407f0013f477de69d8b823f8277e3c3b6525
LidijaLidac <48590238+LidijaLidac@users.noreply.github.com>
2019-03-15 12:53:21 +0100

    Add files via upload

---
+++ RecView.cpp
@@ -0,0 +1,172 @@
+// RecView.cpp : implementation of the RecView class
+//
+
+#include "stdafx.h"
+#include "v10.h"
+
+#include "Set.h"
+#include "Doc.h"
+#include "RecView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// RecView
+
+IMPLEMENT_DYNCREATE(RecView, CRecordView)
+
+BEGIN_MESSAGE_MAP(RecView, CRecordView)
+	//{{AFX_MSG_MAP(RecView)
+	//}}AFX_MSG_MAP
+	// Standard printing commands
+	ON_COMMAND(ID_FILE_PRINT, CRecordView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, CRecordView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, CRecordView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// RecView construction/destruction
+
+RecView::RecView()
+	: CRecordView(RecView::IDD)
+{
+}
+
+RecView::~RecView()
+{
+}
+
+void RecView::DoDataExchange(CDataExchange* pDX)
+{
+	CRecordView::DoDataExchange(pDX);
+	DDX_FieldCheck(pDX, IDC_CHECK1, m_pSet->m_manager, m_pSet);
+	DDX_FieldText(pDX, IDC_EDIT1, m_pSet->m_id, m_pSet);
+	DDX_FieldText(pDX, IDC_EDIT2, m_pSet->m_name, m_pSet);
+
+	//{{AFX_DATA_MAP(RecView)
+	//}}AFX_DATA_MAP
+}
+
+BOOL RecView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	return CRecordView::PreCreateWindow(cs);
+}
+
+void RecView::OnInitialUpdate()
+{
+	m_pSet = &GetDocument()->m_set;
+	CRecordView::OnInitialUpdate();
+	GetParentFrame()->RecalcLayout();
+	ResizeParentToFit();
+
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// RecView printing
+
+BOOL RecView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// default preparation
+	return DoPreparePrinting(pInfo);
+}
+
+void RecView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+}
+
+void RecView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// RecView diagnostics
+
+#ifdef _DEBUG
+void RecView::AssertValid() const
+{
+	CRecordView::AssertValid();
+}
+
+void RecView::Dump(CDumpContext& dc) const
+{
+	CRecordView::Dump(dc);
+}
+
+Doc* RecView::GetDocument() // non-debug version is inline
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(Doc)));
+	return (Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+/////////////////////////////////////////////////////////////////////////////
+// RecView database support
+CRecordset* RecView::OnGetRecordset()
+{
+	return m_pSet;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// RecView message handlers
+
+
+
+void RecView::OnPrint(CDC* pDC, CPrintInfo* pInfo)
+{
+	int hor = pDC->GetDeviceCaps(HORZRES);
+	int ver = pDC->GetDeviceCaps(VERTRES);
+	CSize vel = pDC->GetTextExtent("ID");
+
+	int pomak = vel.cy;
+	
+	int pozId = int(hor*0.1);
+	int pozIme = int(hor*0.2);
+	int pozMan = int(hor*0.5);
+	
+	
+	pDC->TextOut(pozId, pomak, _T("id"));
+	pDC->TextOut(pozIme, pomak, _T("name"));
+	pDC->TextOut(pozMan, pomak, _T("manager"));
+	pomak += 2*vel.cy;
+
+	pDC->MoveTo(pozId, pomak);
+	pDC->LineTo(pozMan*1.5, pomak);
+	pomak += vel.cy;
+
+	Set rs;
+	rs.Open();
+	while (!rs.IsEOF()) {
+		CString s;
+		s.Format(_T("%d"), rs.m_id);
+		pDC->TextOut(pozId,pomak, s);
+
+		pDC->TextOut(pozIme,pomak, rs.m_name);
+
+		if(rs.m_manager)
+		pDC->TextOutA(pozMan,pomak, "x");
+
+		pomak += vel.cy;
+
+		rs.MoveNext();
+	}
+}
+
+
+void RecView::OnPrepareDC(CDC* pDC, CPrintInfo* pInfo)
+{
+	// TODO: Add your specialized code here and/or call the base class
+
+	CRecordView::OnPrepareDC(pDC, pInfo);
+}
+
+
+void RecView::OnDraw(CDC* pDC)
+{
+	// TODO: Add your specialized code here and/or call the base class
+	
+}
